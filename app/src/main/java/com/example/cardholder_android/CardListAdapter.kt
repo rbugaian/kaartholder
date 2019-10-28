8ee85700e9e849b054de856aee3d89718eb4fa7b
Roman Bugaian <bugaian.roman@gmail.com>
2019-10-29 00:14:44 +0100

    63 Add PIN and CVV fields to card creating screen
    Added respective fields and a layout for them.
    Changed input hint styles.
    Remove on card click listener from card list screen.
    Added alternative way to preview private data.

--- app/src/main/java/com/example/cardholder_android/CardListAdapter.kt
+++ app/src/main/java/com/example/cardholder_android/CardListAdapter.kt
@@ -5,7 +5,20 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.cardholder_android.Models.Card
 import kotlinx.android.synthetic.main.card_list_item.view.*
-
+import android.graphics.BlurMaskFilter
+import androidx.core.view.ViewCompat.setLayerType
+import android.os.Build
+import android.text.InputType
+import android.text.method.PasswordTransformationMethod
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.add_card_activity.view.*
+import kotlinx.android.synthetic.main.card_list_item.view.bankAccountView
+import kotlinx.android.synthetic.main.card_list_item.view.cardNameView
+import kotlinx.android.synthetic.main.card_list_item.view.cardNumberView
+import kotlinx.android.synthetic.main.card_list_item.view.expirationDateView
+import kotlinx.android.synthetic.main.card_list_item.view.btnHide
+import kotlinx.android.synthetic.main.card_list_item.view.cardPin
+import kotlinx.android.synthetic.main.card_list_item.view.cardCvv
 
 class CardListAdapter(private val cards: ArrayList<Card>,
                       private val clickListener: (Card) -> Unit) : RecyclerView.Adapter<CardListHolder>() {
@@ -30,6 +43,24 @@ class CardListHolder(v: View) : RecyclerView.ViewHolder(v) {
         itemView.cardNumberView?.text = card.cardNumber
         itemView.bankAccountView?.text = card.bankAccount
         itemView.expirationDateView?.text = card.expDate
-        itemView.setOnClickListener{ clickListener(card)}
+        itemView.btnHide.setOnClickListener{clickListener(card)}
+
+        if (card.isHidden) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                itemView.btnHide.background = ContextCompat.getDrawable(itemView.context, R.drawable.ic_show)
+            } else {
+                itemView.btnHide.setBackgroundDrawable(ContextCompat.getDrawable(itemView.context, R.drawable.ic_show))
+            }
+            itemView.cardPin.transformationMethod = PasswordTransformationMethod()
+            itemView.cardCvv.transformationMethod = PasswordTransformationMethod()
+        } else {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                itemView.btnHide.background = ContextCompat.getDrawable(itemView.context, R.drawable.ic_hide)
+            } else {
+                itemView.btnHide.setBackgroundDrawable(ContextCompat.getDrawable(itemView.context, R.drawable.ic_hide))
+            }
+            itemView.cardPin.transformationMethod = null
+            itemView.cardCvv.transformationMethod = null
+        }
     }
 }
